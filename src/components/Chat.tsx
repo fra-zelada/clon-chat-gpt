c761250186b853949425fbf7724064a1a86bfef6
Francisco <fra.zelada.s@gmail.com>
2023-05-26 13:43:34 -0400

    openai api added

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -16,7 +16,7 @@ export const Chat = () => {
     <div className='flex flex-col h-full flex-1 pl-64'>
       <main>
         {messages.map(({ id, ia, message }) => (
-          <Message key={id} ia={ia} message={message} />
+          <Message key={`${id}`} ia={ia} message={message} />
         ))}
 
         <p ref={dummy} />
--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { OpenAIApi, Configuration } from 'openai'
 
 type Data = {
   message?: string
@@ -18,34 +19,32 @@ export default async function handler(
   }
 
   try {
-    const response = await fetch('https://api.openai.com/v1/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization:
-          `Bearer ${process.env.CHAT_API_KEY}`
-      },
-      body: JSON.stringify({
-        model: 'text-davinci-003',
-        prompt: `Simula que eres la i.a de ChatGPT y da respuesta al siguiente prompt:\n\n${prompt}?`,
-        temperature: 0.7,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0
-      })
+    const config = new Configuration({
+      apiKey: process.env.CHAT_API_KEY,
     })
+    const openai = new OpenAIApi(config);
+    
 
-    if (!response.ok) {
-      return res.status(500).json({ message: 'OpenAI API Error' })
-    }
-    const json = await response.json()
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{role: "user", content: prompt}],
 
-    // const responseMessage = json.choices[0].text.replace(/(\r\n|\n|\r)/gm, ' ')
-    const responseMessage = json.choices[0].text.trim()
+      max_tokens: 200,
+      temperature: 0.7,
+      top_p: 1,   
+      frequency_penalty: 1,presence_penalty: 1,
+             
+   });
+   
+   
 
-    return res.status(200).json({ response: responseMessage })
+
+    if(completion.data.choices[0].message)
+    return res.status(200).json({ response: completion.data.choices[0].message?.content })
+    else throw new Error("No response");
+    
   } catch (error) {
+    console.log(error)
     return res.status(400).json({ message: 'Internal Error' })
   }
 }
