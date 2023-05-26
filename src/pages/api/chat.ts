001c6b9b0944ab799b3013318e0f9ad5e70193df
Francisco <fra.zelada.s@gmail.com>
2023-05-26 14:37:39 -0400

    eslint corrections

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -20,29 +20,23 @@ export default async function handler(
 
   try {
     const config = new Configuration({
-      apiKey: process.env.CHAT_API_KEY,
+      apiKey: process.env.CHAT_API_KEY
     })
-    const openai = new OpenAIApi(config);
-    
+    const openai = new OpenAIApi(config)
 
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: [{role: "user", content: prompt}],
+      messages: [{ role: 'user', content: prompt }],
 
       max_tokens: 200,
       temperature: 0.7,
-      top_p: 1,   
-      frequency_penalty: 1,presence_penalty: 1,
-             
-   });
-   
-   
+      top_p: 1,
+      frequency_penalty: 1,
+      presence_penalty: 1
 
+    })
 
-    if(completion.data.choices[0].message)
-    return res.status(200).json({ response: completion.data.choices[0].message?.content })
-    else throw new Error("No response");
-    
+    if (completion.data.choices[0].message) { return res.status(200).json({ response: completion.data.choices[0].message?.content }) } else throw new Error('No response')
   } catch (error) {
     console.log(error)
     return res.status(400).json({ message: 'Internal Error' })
